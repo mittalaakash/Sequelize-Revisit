9aa1c91facf6c9e51b6ea324d2780c8b71e34316
Mridul <mit.ash.619@gmail.com>
2023-05-30 02:42:06 +0530

    initital commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+  res.set({ 'Content-Type': 'text/html' });
+  res.status(200).send('<h1>hello</h1>');
+});
+
+app.listen(3000, () => {
+  console.log('server is listening on http://localhost:3000');
+});
